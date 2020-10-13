309794492dc05a0e1cccdf5a0f1159f4935c58d5
SteffiKeranJ <steffikeranj@gmail.com>
2020-10-14 00:47:45 +0530

    Add controllers and views for creating and deleting comments

--- db/migrate/20201013183143_create_comments.rb
+++ db/migrate/20201013183143_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.string, :name
+      t.string :name
       t.text :comment
 
       t.timestamps
