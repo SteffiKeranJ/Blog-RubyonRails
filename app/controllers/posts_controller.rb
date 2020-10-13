71675c2aa8a241321d59464c0e77056886e364b2
SteffiKeranJ <steffikeranj@gmail.com>
2020-10-13 23:54:24 +0530

    Add controller and view for delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -32,7 +32,12 @@ class PostsController < ApplicationController
 
     def edit
         @post = Post.find(params[:id])
-        
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to posts_path
     end
 
     private
